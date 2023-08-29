818e50bcb0ded259b60b39c87a6af0553e330988
Mollis <ram.takemura@gmail.com>
2023-08-28 18:48:36 -0700

    защита от удаления данных из за изменения бренча

--- index.php
+++ index.php
@@ -16,25 +16,29 @@ $read = new Read();
             <h3 class='top-mod__subtitle top-mod__subtitle_modify'>sdfsfsdfsfdsfds</h3>
         </div>
     </aside>
+    <div class="news-container">
     <?php
         foreach($read->allNews() as $new):
     ?>
-    <div class="news-block news-block_modify">
-        <img src="<?= $new["img"]?>" alt=""
-            class="news-block__img news-block__img_modify"></img>
-        <div class="news-block__description news-block__description_modify">
-            <h3 class="news-block__subtitle news-block__subtitle_modify"><?= $new["title"]?></h3>
-            <p>
-                <?= $new["content"]?>
-            </p>
-            <div class="div"><?= $new["date"]?></div>
-            <div class="div"><?= $new["login"]?></div>
-            <span onclick="location.href='/view/news-page.php?news-=<?= $new['id'] ?>'" class="open-news-btn open-news-btn_modify">Читать статью</span>
+    
+        <div class="news-block news-block_modify">
+            <img src="<?= $new["img"]?>" alt=""
+                class="news-block__img news-block__img_modify"></img>
+            <div class="news-block__description news-block__description_modify">
+                <h3 class="news-block__subtitle news-block__subtitle_modify"><?= $new["title"]?></h3>
+                <p>
+                    <?= $new["content"]?>
+                </p>
+                <div class="div">Автор: <?= $new["login"]?></div>
+                <div class="news-date">Дата публикации: <?= $new["date"]?></div>
+                <span onclick="location.href='/view/news-page.php?news-=<?= $new['id'] ?>'" class="open-news-btn open-news-btn_modify">Читать статью</span>
+            </div>
         </div>
-    </div>
+    
     <?php
         endforeach
     ?>
+    </div>
 </section>
 
 <?php
