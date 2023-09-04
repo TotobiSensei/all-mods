ce1afa316b45572dcee55d8fa276483fbc91c7a2
Mollis <ram.takemura@gmail.com>
2023-09-04 11:36:31 -0700

    файл в который импортируються все модули. Этот файл в дальнейшем веб пак использует как стартовую точку сборки

---
+++ assets/js/modul-colector.js
@@ -0,0 +1,9 @@
+import modalAction from "./modules/modal-action";
+import userIMg from "./modules/user_img";
+import paginationReplacement from "./modules/pagination-replacement";
+
+window.addEventListener('DOMContentLoaded', () => {
+    modalAction();
+    userIMg();
+    paginationReplacement();
+})
