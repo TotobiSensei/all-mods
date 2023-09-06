53f824d0b5274b93448c36d7b0bf641f9939dd5e
Mollis <ram.takemura@gmail.com>
2023-09-05 20:48:54 -0700

    added new methods for message checking

--- assets/js/modul-colector.js
+++ assets/js/modul-colector.js
@@ -1,12 +1,12 @@
 import modalAction from "./modules/modal-action";
 import userIMg from "./modules/user_img";
 import paginationReplacement from "./modules/pagination-replacement";
-import checkMessageStatus from "./modules/check-message";
+import messageAction from "./modules/message-action";
 
 window.addEventListener('DOMContentLoaded', () => {
     modalAction();
     userIMg();
     paginationReplacement();
 
-    checkMessageStatus(".message-list");
+    messageAction(".message-list");
 })
