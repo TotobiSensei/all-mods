e71acd03b0d93d5dd87ba217332fc75e77f3d3bf
Mollis <ram.takemura@gmail.com>
2023-09-07 19:47:27 -0700

    added new chat-scroll methods

--- assets/js/modul-colector.js
+++ assets/js/modul-colector.js
@@ -3,12 +3,15 @@ import userIMg from "./modules/user_img";
 import paginationReplacement from "./modules/pagination-replacement";
 import messageAction from "./modules/message-action";
 import keyModyfier from "./modules/send-message-methods";
+import chatObserv from "./modules/chat-scrolling-action";
 
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
     modalAction();
     userIMg();
     paginationReplacement();
 
     messageAction(".message-list");
-    keyModyfier('.bottom form')
+    keyModyfier(".bottom form")
+    chatObserv(".message-list")
+
 })
