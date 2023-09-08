f3ad2a7ae2b942862faba767edbdf7b96e150711
Mollis <ram.takemura@gmail.com>
2023-09-07 20:41:04 -0700

    coe refactoring

--- assets/js/modul-colector.js
+++ assets/js/modul-colector.js
@@ -4,8 +4,16 @@ import paginationReplacement from "./modules/pagination-replacement";
 import messageAction from "./modules/message-action";
 import keyModyfier from "./modules/send-message-methods";
 import chatObserv from "./modules/chat-scrolling-action";
+import checkVisibleMessages from "./modules/Up-date-messages";
+
+window.addEventListener("load", checkVisibleMessages);
 
 window.addEventListener("DOMContentLoaded", () => {
+    // Отправляем AJAX-запросы для видимых сообщений при загрузке страницы
+// Отправляем AJAX-запросы для видимых сообщений при скроллинге в области "message-list"
+document.getElementById("message-list").addEventListener("scroll", checkVisibleMessages);
+
+
     modalAction();
     userIMg();
     paginationReplacement();
